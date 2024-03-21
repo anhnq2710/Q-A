9adbc9ee00832466ac0ce25d494b536271ea513c
Quốc Anh <132922557+anhnq2710@users.noreply.github.com>
2024-03-22 01:41:11 +0700

    Update baithi.js

--- baithi.js
+++ baithi.js
@@ -40,10 +40,28 @@ function submitAnswer() {
     }
     totalQuestions++;
 
+    var question3Answer = document.querySelector('input[name="answer3"]:checked');
+    if (question3Answer && question3Answer.value === "D") {
+        correctAnswers++;
+    }
+    totalQuestions++;
+
+    var question4Answer = document.querySelector('input[name="answer4"]:checked');
+    if (question4Answer && question4Answer.value === "C") {
+        correctAnswers++;
+    }
+    totalQuestions++;
+
+    var question5Answer = document.querySelector('input[name="answer5"]:checked');
+    if (question5Answer && question5Answer.value === "A") {
+        correctAnswers++;
+    }
+    totalQuestions++;
     var score = (correctAnswers / totalQuestions) * 100;
 
     window.location.href = 'ketqua.html?correct=' + correctAnswers + '&total=' + totalQuestions + '&score=' + score +
-    '&answer1=' + encodeURIComponent(question1Answer.value) + '&answer2=' + encodeURIComponent(question2Answer.value);
+    '&answer1=' + encodeURIComponent(question1Answer.value) + '&answer2=' + encodeURIComponent(question2Answer.value) + '&answer3=' + encodeURIComponent(question3Answer.value)
+    + '&answer4=' + encodeURIComponent(question4Answer.value)+ '&answer5=' + encodeURIComponent(question5Answer.value);
 }
 
 document.getElementById("submit").addEventListener("click", submitAnswer);
